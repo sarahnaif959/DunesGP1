a6d72797279aa6296e46142e97316768da12adb3
sarahnaif959 <90639052+sarahnaif959@users.noreply.github.com>
2026-02-16 20:14:53 +0300

    Location of dune

--- server.js
+++ server.js
@@ -64,8 +64,7 @@ function baseName(fileName) {
 app.use(cors());
 app.use(express.json());
 app.use((req, _res, next) => { console.log("➡️", req.method, req.url); next(); });
-app.use(express.static(path.join(__dirname, "public")));
-
+app.use(express.static("public"));
 // Optional but explicit:
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
